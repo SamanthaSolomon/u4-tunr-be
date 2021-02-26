316848be05e1646453811c8f7deb7ab379295b31
Samantha Solomon <sleahsolomon@gmail.com>
2021-02-26 12:15:55 -0500

    added favorite column and seed data

---
+++ db/migrate/20210226170126_add_favorite_column.rb
@@ -0,0 +1,5 @@
+class AddFavoriteColumn < ActiveRecord::Migration[6.1]
+  def change
+    add_column :songs, :favorite, :boolean
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ 10.times do
+    Song.create([
+        {artist: Faker::Music::RockBand.name, 
+        title: Faker::Music::RockBand.song, 
+        time: Faker::Time.between(from: Time.now - 7, to: Time.now - 6),
+        favorite: false}
+    ])
+end
